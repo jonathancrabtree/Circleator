e20c34384169cd34854c0ec6929e2b51fa4c7496
Jonathan Crabtree <jonathancrabtree@gmail.com>
2015-01-21 16:13:28 -0500

    Update compile command for Ubuntu 14.01.1 LTS

--- util/samtools/make.sh
+++ util/samtools/make.sh
@@ -1,7 +1,6 @@
-#!/bin/tcsh
+#!/bin/bash
 
 # compilation command for Ubuntu Linux
-setenv CFLAGS "-I/usr/include/glib-2.0 -I/usr/include/glib-2.0/glib -I/usr/lib/glib-2.0/include"
-setenv SAMTOOLS_DIR samtools-0.1.19
-gcc $CFLAGS -I${SAMTOOLS_DIR} -L${SAMTOOLS_DIR} -L${SAMTOOLS_DIR}/bcftools bam_hmp_util.c bam_get_coverage.c -o bam_get_coverage -lz -lbam -lgd -lgobject-2.0 -lgmodule-2.0
-
+export CFLAGS=`pkg-config --cflags --libs glib-2.0`
+export SAMTOOLS=../samtools-0.1.19
+gcc bam_hmp_util.c bam_get_coverage.c  $CFLAGS -L$SAMTOOLS -I$SAMTOOLS -lbam -o bam_get_coverage -lz -lgobject-2.0 -lgmodule-2.0 -lpthread
