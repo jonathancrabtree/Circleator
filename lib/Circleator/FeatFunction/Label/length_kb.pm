7ebe9c74f50e6751399c8e4bc74618d44ea06b12
Jonathan Crabtree <jonathancrabtree@gmail.com>
2017-09-08 11:51:05 -0400

    Partial fix for #32 (the negative length part.)

--- lib/Circleator/FeatFunction/Label/length_kb.pm
+++ lib/Circleator/FeatFunction/Label/length_kb.pm
@@ -6,7 +6,13 @@ sub get_function {
     my $f = shift;
     my $start = $f->start();
     my $end = $f->end();
-    my $length = $end - $start + 1;
+    my $length = undef;
+    if ($end >= $start) {
+	$length = $end - $start + 1;
+    } else {
+	my $seqlen = $f->entire_seq->length();
+	$length = ($seqlen - $start) + $end + 1;
+    }
     return sprintf("%0.1f", $length/1000) . "kb";
   };
 }
