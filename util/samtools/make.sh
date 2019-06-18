f5dfe7c2872e71aec0bf55d5618080ca5ae57090
Jonathan Crabtree <jonathancrabtree@gmail.com>
2019-06-18 14:39:03 -0400

    Update compilation command for Ubuntu 16.04LTS and samtools-1.7

--- util/samtools/make.sh
+++ util/samtools/make.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# compilation command for Ubuntu Linux
+# compilation command for Ubuntu 16.04LTS
 export CFLAGS=`pkg-config --cflags --libs glib-2.0`
-export SAMTOOLS=../samtools-0.1.19
-gcc bam_hmp_util.c bam_get_coverage.c  $CFLAGS -L$SAMTOOLS -I$SAMTOOLS -lbam -o bam_get_coverage -lz -lgobject-2.0 -lgmodule-2.0 -lpthread
+export SAMTOOLS=/opt/samtools-1.7
+export HTSLIB=/opt/samtools-1.7/htslib-1.7
+gcc bam_hmp_util.c bam_get_coverage.c  $CFLAGS -L$SAMTOOLS -L$HTSLIB -I$SAMTOOLS -I$HTSLIB -lst -lhts -lbam -o bam_get_coverage -llzma -lbz2 -lz -lgobject-2.0 -lgmodule-2.0 -lpthread
